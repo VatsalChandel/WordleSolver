6cf07033c3fe236f010ef1df4fd8b7b19df5cd26
ChandelVatsal <68132483+ChandelVatsal@users.noreply.github.com>
2022-04-24 23:24:31 -0700

    Basic solver without UI

---
+++ Solver.py
@@ -0,0 +1,49 @@
+# Always a five letter word
+# Words can be green, yellow, or black 
+# Only have six tries 
+
+
+attempt_number = 0
+words = []
+
+with open ("AllWords.txt") as f:
+    for line in f:
+        words.append(line.strip())
+
+print("Words are loaded and ready")
+    
+while (attempt_number <= 6):
+    print()
+    user_guess = input("\nword: ").lower()
+    print()
+    print("g for green, y for yellow, and b for grey")
+    from_user = input("\nWhat was the outcome: ").lower()
+    if from_user == "ggggg":
+        print("Great guess, tries: ", attempt_number+1)
+        break
+    
+    temp_tuple = tuple(words)
+    for word in temp_tuple:
+        for i in range(5):
+            
+            if from_user[i] == 'b' and user_guess[i] in word:
+                words.remove(word)
+                break
+            elif from_user[i] == 'g' and user_guess[i] != word[i]:
+                words.remove(word)
+                break
+            elif from_user[i] == 'y' and user_guess[i] not in word:
+                words.remove(word)
+                break
+            elif from_user[i] == 'y' and user_guess[i] == word[i]:
+                words.remove(word)
+                break
+            
+    counter = 0
+    for word in words:
+        print(word, end=", ")
+        counter += 1
+        if counter == 8:
+            print("")
+            counter = 0
+        
